657d7027e74ad6ef7aeb5b7484a1cbe42ffb4e92
hakumaku <gentlebuuny@gmail.com>
2021-08-03 16:49:48 +0900

    Update script
    - Add old commits filter
    - Add # of lines filter

--- script/extract_candidate.py
+++ script/extract_candidate.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import sys
+from datetime import datetime
 from os import getcwd
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, Generator, List, Tuple
 
 import git
 
 KEYWORDS = ("fix", "bug")
-DIRECTORY = ("src", "test")
+DIRECTORY = ("src", "tests")
 EXTENSION = (".cpp", ".c")
+AGE = 2
+DIFF_CONTENTS = 100
+NUM_FILE = 8
 
 
 def check_message(commit_msg: str):
@@ -18,6 +22,11 @@ def check_message(commit_msg: str):
 
 def check_files(commit_files: Dict[str, Dict[str, int]]):
     files = commit_files.keys()
+
+    # If the number of files are too big, get rid of it from candidates.
+    if len(files) > NUM_FILE:
+        return False
+
     # Consider modified files inside DIRECTORY only.
     for d in DIRECTORY:
         if not any(file.startswith(d) for file in files):
@@ -27,11 +36,21 @@ def check_files(commit_files: Dict[str, Dict[str, int]]):
     if not any(Path(file).suffix in EXTENSION for file in files):
         return False
 
-    # If the number of files are too big, get rid of it from candidates.
-    return len(files) < 8
+    # Check the number of inserted lines and deleted lines.
+    inserted_lines = sum(
+        commit_files[src]["insertions"]
+        for src in (file for file in files if Path(file).suffix in EXTENSION)
+    )
+    deleted_lines = sum(
+        commit_files[src]["deletions"]
+        for src in (file for file in files if Path(file).suffix in EXTENSION)
+    )
+    return (inserted_lines + deleted_lines) < DIFF_CONTENTS
 
 
-def iter_pair_commits(repo: git.Repo):
+def iter_pair_commits(
+    repo: git.Repo,
+) -> Generator[Tuple[git.Commit, git.Commit], None, None]:
     it = repo.iter_commits("master")
     try:
         current_commit = next(it)
@@ -48,7 +67,24 @@ def main(argv: List[str]):
     path = Path(argv[0])
     repo = git.Repo(str(path))
 
-    for current_commit, next_commit in iter_pair_commits(repo):
+    now = datetime.now()
+    time_threshold = 365 * AGE
+    count = 0
+
+    for index, (current_commit, next_commit) in enumerate(
+        iter_pair_commits(repo), start=1
+    ):
+        if index % 100 == 0:
+            print(f"Iterating {index} commits")
+
+        # Consider commits in the last two years.
+        commit_datetime = datetime.fromtimestamp(current_commit.committed_date)
+        if (now - commit_datetime).days > time_threshold:
+            print(
+                f"Commits made more than {AGE} years ago are not considered. Stop iterating commits."
+            )
+            break
+
         if not check_message(str(current_commit.message)):
             continue
         if not check_files(current_commit.stats.files):
@@ -56,11 +92,14 @@ def main(argv: List[str]):
 
         p = Path(getcwd()) / current_commit.hexsha
         p.mkdir(parents=True)
-        with open(p / "commit-message", "w+", encoding='utf-8') as fp:
+        with open(p / "commit-message", "w+", encoding="utf-8") as fp:
             fp.write(str(current_commit.message))
-        with open(p / "diff.patch", "w+", encoding='utf-8') as patch_file:
+        with open(p / "diff.patch", "w+", encoding="utf-8") as patch_file:
             for patch in current_commit.diff(next_commit, create_patch=True):
-                patch_file.write(patch.diff.decode("utf-8"))
+                patch_file.write(patch.diff.decode("utf-8", errors="ignore"))
+        # Progress info
+        count += 1
+        print(f"Current # of candidate: {count}")
 
 
 if __name__ == "__main__":
