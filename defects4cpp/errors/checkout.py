349c5d319c0c8aa21e411cc10cd633469ea2db23
haku <gentlebuuny@gmail.com>
2021-11-04 09:25:08 +0900

    Refactor: move code to taxonomy.py

--- defects4cpp/errors/checkout.py
+++ defects4cpp/errors/checkout.py
@@ -3,6 +3,7 @@ from typing import List, Tuple, Union
 
 import git
 from errors.common.exception import DppError
+import taxonomy
 
 
 class DppGitError(DppError):
@@ -12,7 +13,7 @@ class DppGitError(DppError):
 class DppGitCloneError(DppGitError):
     def __init__(
         self,
-        metadata: "taxonomy.MetaData",
+        metadata: taxonomy.MetaData,
         path: str,
         command: Union[List[str], Tuple[str, ...], str],
         status: Union[str, int, None, Exception],
@@ -41,7 +42,7 @@ class DppGitCloneError(DppGitError):
 
 
 class DppGitWorktreeError(DppGitError):
-    def __init__(self, repo: git.Repo, path: str, defect: "taxonomy.Defect"):
+    def __init__(self, repo: git.Repo, path: str, defect: taxonomy.Defect):
         self.repo = repo
         self.path = path
         self.defect = defect
@@ -58,7 +59,7 @@ class DppGitWorktreeError(DppGitError):
 
 
 class DppGitCheckoutInvalidRepositoryError(DppGitError):
-    def __init__(self, repo: git.Repo, path: str, defect: "taxonomy.Defect"):
+    def __init__(self, repo: git.Repo, path: str, defect: taxonomy.Defect):
         self.repo = repo
         self.path = path
         self.defect = defect
@@ -75,7 +76,7 @@ class DppGitCheckoutInvalidRepositoryError(DppGitError):
 
 
 class DppGitCheckoutError(DppGitError):
-    def __init__(self, repo: git.Repo, path: str, defect: "taxonomy.Defect"):
+    def __init__(self, repo: git.Repo, path: str, defect: taxonomy.Defect):
         self.repo = repo
         self.path = path
         self.defect = defect
