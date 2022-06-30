7ccfbefbc7677d7848a1c12d99326611f83ef470
HansolChoe <hansol614@gmail.com>
2022-06-30 09:06:29 +0900

    Add partial implemetnation of readme generator

---
+++ readme_generator.py
@@ -0,0 +1,8 @@
+from defects4cpp.taxonomy import Taxonomy
+
+def generate_readme(taxonomy: Taxonomy, input_file_path = "README.rst.template", output_file_path = "REAMDE.rst"):
+    # with open(input_file_path):
+    pass
+
+if __name__ == "__main__":
+    generate_readme(Taxonomy())
