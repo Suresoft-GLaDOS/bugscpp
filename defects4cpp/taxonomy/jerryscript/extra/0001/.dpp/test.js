5d280126b8f95f0fa1c81628b09f2cf2f9dcd8dc
icechocola05 <icechocola05@gmail.com>
2022-07-14 15:04:09 +0900

    Add defect: jerryscript-1

---
+++ defects4cpp/taxonomy/jerryscript/extra/0001/.dpp/test.js
@@ -0,0 +1,23 @@
+function main() {
+var v2 = new Float64Array(63797);
+var v4 = "d".constructor;
+var v6 = [1337,1337,1337,1337];
+var v7 = [];
+var v8 = {constructor:v6,a:v7};
+var v9 = v8.a;
+var v12 = 0;
+v9.toString = v4;
+var v14 = new Int16Array();
+do {
+    function v16(v17,v18,v19) {
+        'use strict'
+        var v20 = Int16Array.toLocaleString();
+        var v22 = eval(v20);
+    }
+    var v24 = new Promise(v16);
+    var v25 = v12 + 1;
+    v12 = v25;
+    v7[v25] = v14;
+} while (v12 < 1337);
+}
+main();
