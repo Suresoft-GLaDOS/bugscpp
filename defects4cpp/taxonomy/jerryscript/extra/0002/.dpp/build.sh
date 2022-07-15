89a5a946e72202c937167edae2c7ab85d6710b03
icechocola05 <icechocola05@gmail.com>
2022-07-15 16:13:06 +0900

    Add defect: jerryscript-2

---
+++ defects4cpp/taxonomy/jerryscript/extra/0002/.dpp/build.sh
@@ -0,0 +1,104 @@
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es.next \
+        --function-to-string=on \
+        --promise-callback=on \
+        --builddir=/home/workspace/build/tests/unittests-es.next \
+        --install=/home/workspace/build/tests/unittests-es.next/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --doctests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --profile=es.next \
+        --function-to-string=on \
+        --promise-callback=on \
+        --builddir=/home/workspace/build/tests/doctests-es.next \
+        --install=/home/workspace/build/tests/doctests-es.next/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --builddir=/home/workspace/build/tests/unittests-es5.1 \
+        --install=/home/workspace/build/tests/unittests-es5.1/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --doctests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --profile=es5.1 \
+        --builddir=/home/workspace/build/tests/doctests-es5.1 \
+        --install=/home/workspace/build/tests/doctests-es5.1/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --cmake-param=-DFEATURE_INIT_FINI=ON \
+        --builddir=/home/workspace/build/tests/unittests-es5.1-init-fini \
+        --install=/home/workspace/build/tests/unittests-es5.1-init-fini/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --jerry-math=on \
+        --builddir=/home/workspace/build/tests/unittests-es5.1-math \
+        --install=/home/workspace/build/tests/unittests-es5.1-math/local \
+        --compile-flag="$1"
+python /home/workspace/tools/build.py \
+        --profile=es2015-subset \
+        --lto=off \
+        --error-messages=on \
+        --debug \
+        --strip=off \
+        --logging=on \
+        --compile-flag=-fsanitize=address \
+        --compile-flag=-m32 \
+        --compile-flag=-fno-omit-frame-pointer \
+        --compile-flag=-fno-common\
+        --stack-limit=20 \
+        --system-allocator=on --linker-flag=-fuse-ld=gold \
+        --compile-flag="$1"
