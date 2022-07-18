80148425f4eeac3caee5179f393d6dbf8048fb46
kseo <kseo@suresofttech.com>
2022-07-18 14:27:39 +0900

    Add defect : jerryscript - 8

---
+++ defects4cpp/taxonomy/jerryscript/extra/0008/.dpp/build.sh
@@ -0,0 +1,96 @@
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --jerry-math=on \
+        --builddir=/home/workspace/build/tests/unittests-es5.1-math \
+        --install=/home/workspace/build/tests/unittests-es5.1-math/local
+
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --cmake-param=-DFEATURE_INIT_FINI=ON \
+        --builddir=/home/workspace/build/tests/unittests-es5.1-init-fini \
+        --install=/home/workspace/build/tests/unittests-es5.1-init-fini/local
+
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --doctests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --profile=es5.1 \
+        --builddir=/home/workspace/build/tests/doctests-es5.1 \
+        --install=/home/workspace/build/tests/doctests-es5.1/local
+
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es5.1 \
+        --builddir=/home/workspace/build/tests/unittests-es5.1 \
+        --install=/home/workspace/build/tests/unittests-es5.1/local
+
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --doctests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --profile=es.next \
+        --function-to-string=on \
+        --promise-callback=on \
+        --builddir=/home/workspace/build/tests/doctests-es.next \
+        --install=/home/workspace/build/tests/doctests-es.next/local
+
+python  /home/workspace/tools/build.py \
+        --lto=off \
+        --unittests=on \
+        --jerry-cmdline=off \
+        --error-messages=on \
+        --snapshot-save=on \
+        --snapshot-exec=on \
+        --vm-exec-stop=on \
+        --vm-throw=on \
+        --line-info=on \
+        --mem-stats=on \
+        --profile=es.next \
+        --function-to-string=on \
+        --promise-callback=on \
+        --builddir=/home/workspace/build/tests/unittests-es.next \
+        --install=/home/workspace/build/tests/unittests-es.next/local
+
+python  /home/workspace/tools/build.py \
+        --clean --debug --compile-flag=-fsanitize=address \
+        --compile-flag=-m32 --compile-flag=-g --strip=off \
+        --lto=off --logging=on --line-info=on \
+        --error-message=on --system-allocator=on \
+        --stack-limit=20
