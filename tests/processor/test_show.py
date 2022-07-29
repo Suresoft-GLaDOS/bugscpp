da47c831dca116a42be28e4b128aa702c782ebd9
kseo <kseo@suresofttech.com>
2022-07-29 14:31:04 +0900

    Cover help.py, show.py.

--- tests/processor/test_show.py
+++ tests/processor/test_show.py
@@ -1,6 +1,16 @@
 from defects4cpp.command import CommandList
-
+from defects4cpp.command import ShowCommand
 
 def test_check_show_attr():
     commands = CommandList()
     assert "show" in commands
+
+def test_check_show_help():
+    showCommand = ShowCommand()
+    showCommand.help
+
+def test_check_show_run():
+    showCommand = ShowCommand()
+    showCommand.run()
+
+
