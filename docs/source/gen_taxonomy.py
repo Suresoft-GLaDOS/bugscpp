fc5a2401af9fdfb35cf13372abd5802a49c9f7a4
crAy <crAyCoding@gmail.com>
2022-08-23 17:04:41 +0900

    Improve gen_taxonomy.py

--- docs/source/gen_taxonomy.py
+++ docs/source/gen_taxonomy.py
@@ -19,11 +19,15 @@ A List of Defect Taxonomy
 =========================
 
 .. list-table:: Defect Taxonomy
-   :widths: 40 40
    :header-rows: 1
 
    * - Project
-     - # of bugs
+     - bug_ID
+     - files
+     - linesAdd
+     - linesDel
+     - methods
+     - sizeInLines
 """
 
 rows = []
@@ -32,8 +36,46 @@ for entry in (path / taxonomy_path).iterdir():
         continue
     with open(entry / "meta.json") as fp:
         meta = json.load(fp)
-    rows.append(f"   * - {entry.name}\n")
-    rows.append(f"     - {len(meta['defects'])}\n")
+        num_of_defects = len(meta['defects'])
+    for index in range(num_of_defects):
+        patch_name = str(index+1).zfill(4) + '-buggy.patch'
+        with open(entry / 'patch' / patch_name) as buggy:
+            buggy_lines = buggy.readlines()
+            bug_id = index+1
+            files_changed = 0
+            lines_add = 0
+            lines_del = 0
+            methods = 0
+            for line in buggy_lines:
+                lines = line.split()
+                if len(lines) == 0:
+                    continue
+
+                # check the number of files , added_lines , deleted_lines
+                if len(lines) > 2 and lines[2] == 'changed,':
+                    files_changed = lines[0]
+                    # buggy patch with insertion and deletion
+                    if len(line.split()) == 7:
+                        lines_add = int(lines[3])
+                        lines_del = int(lines[5])
+                    # buggy patch only with deletion
+                    elif line[-3] == '-':
+                        lines_del = int(lines[3])
+                    # buggy patch only with insertion
+                    elif line[-3] == '+':
+                        lines_add = int(lines[3])
+
+                # check methods in patch
+                if lines[0] == '@@':
+                    methods += 1
+
+        rows.append(f"   * - {entry.name}\n")
+        rows.append(f"     - {bug_id}\n")
+        rows.append(f"     - {files_changed}\n")
+        rows.append(f"     - {lines_add}\n")
+        rows.append(f"     - {lines_del}\n")
+        rows.append(f"     - {methods}\n")
+        rows.append(f"     - {lines_add + lines_del}\n")
 
 with open(path / "docs/source/taxonomy.rst", "w+") as fp:
     fp.write(f"{table}{''.join(rows)}")
